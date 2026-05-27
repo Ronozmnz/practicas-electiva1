abfd996e473f7677f205dc93fee022994e1b2b42
Ronozmnz <ronaldrativa18@gmail.com>
2026-05-27 18:32:57 -0400

    Commit inicial: Colección de prácticas de Electiva I

---
+++ js/clase1.js
@@ -0,0 +1,16 @@
+console.log("Esta funcion imprime en pantalla")
+
+let variable = "let declara una variable"
+
+const constante = "const declara una constante que no se puede modificar"
+
+let arr = [1,2,3,4,"hola",true,false,{id:1,nombre:"Ronald"},[1,2,3]];
+
+let objetc = {id:1,
+              nombre:"Ronald",
+            }
+
+
+
+            
+
